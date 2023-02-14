486bdf4d97d17347dd6a04ca1869ef853a9aa601
Punit <punitsureka1@gmail.com>
2023-02-14 14:54:59 +0530

    added update profile and expertise

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -16,7 +16,7 @@ router.get('/',authenticateToken, async(req,res) => {
     }
 })
 
-router.get('/interests',authenticateToken, async(req,res) => {
+router.post('/interests',authenticateToken, async(req,res) => {
     try{
         const decoded = jwt.verify(req.cookies['refresh token'], process.env.REFRESH_TOKEN_SECRET); 
         const username = decoded.username;
@@ -26,12 +26,41 @@ router.get('/interests',authenticateToken, async(req,res) => {
         [username, rows[0]['topic_id']]);
         
         res.json({username : username, topic: newUser});   
-        // const users = await pool.query('SELECT * FROM users');
+    } catch(error){
+        res.status(500).json({error:error.message});
+    }
+})
+
+router.post('/update', async(req,res) =>{
+    try{
+        const decoded = jwt.verify(req.cookies['refresh token'], process.env.REFRESH_TOKEN_SECRET); 
+        const username = decoded.username;
+
+        const updateUser = await pool.query('INSERT INTO hosts (username, bio, photo, website, linkedin) VALUES ($1, $2, $3, $4, $5) RETURNING *', 
+        [username, req.body.bio, req.body.photo, req.body.website, req.body.linkedin]);
+
+        res.json({users:updateUser.rows[0]})
+    } catch(error){
+        res.status(500).json({error:error.message})
+    }
+})
+
+router.post('/expertise',authenticateToken, async(req,res) => {
+    try{
+        const decoded = jwt.verify(req.cookies['refresh token'], process.env.REFRESH_TOKEN_SECRET); 
+        const username = decoded.username;
+        const hostid = await pool.query('SELECT host_id FROM hosts WHERE username = $1', [username])
+        const { rows } = await pool.query('SELECT topic_id FROM topics WHERE topic = $1', [req.body.expertise])
+        
+        const newUser = await pool.query('INSERT INTO expertise (host_id, topic_id) VALUES ($1, $2) RETURNING *', 
+        [hostid.rows[0]['host_id'], rows[0]['topic_id']]);
+
+        res.send(newUser)
+        
     } catch(error){
         res.status(500).json({error:error.message});
     }
 })
 
 
-
-export default router; 
+export default router;
